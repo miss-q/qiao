4afeebbe94b88d938b6b765fb1523ac946ad4293
qiaochengjie <1392090625@qq.com>
2020-03-24 18:59:12 +0800

    第二次提交作业

--- mavenjava/src/main/java/com/qiaochengjie/text/TextMain.java
+++ mavenjava/src/main/java/com/qiaochengjie/text/TextMain.java
@@ -2,6 +2,6 @@ package com.qiaochengjie.text;
 
 public class TextMain {
 public static void main(String[] args) {
-	System.out.println("helloword qiao十四岁!");
+	System.out.println("helloword qiao十四岁我问问!");
 }
 }
